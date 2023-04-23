070630abbaa2eafd55fe03979cac4ee61a7dc6c8
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-04-23 15:56:34 -0300

    OrderItem, Many-to-Many Association with Extra Attributes pt2

--- src/main/java/com/udemy/cursojava/config/TestConfig.java
+++ src/main/java/com/udemy/cursojava/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.udemy.cursojava.entities.Category;
 import com.udemy.cursojava.entities.Order;
+import com.udemy.cursojava.entities.OrderItem;
 import com.udemy.cursojava.entities.Product;
 import com.udemy.cursojava.entities.User;
 import com.udemy.cursojava.entities.enums.OrderStatus;
 import com.udemy.cursojava.repositories.CategoryRepository;
+import com.udemy.cursojava.repositories.OrderItemRepository;
 import com.udemy.cursojava.repositories.OrderRepository;
 import com.udemy.cursojava.repositories.ProductRepository;
 import com.udemy.cursojava.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+	
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
--- src/main/java/com/udemy/cursojava/entities/OrderItem.java
+++ src/main/java/com/udemy/cursojava/entities/OrderItem.java
@@ -3,6 +3,7 @@ package com.udemy.cursojava.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.udemy.cursojava.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable {
 
 	//Id composto
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -32,6 +33,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
