dfd56e2cfe98635f67de5732fbef5b56bc1ec695
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-04-25 18:29:39 -0300

    Exception Handling - Delete

--- src/main/java/com/udemy/cursojava/services/UserService.java
+++ src/main/java/com/udemy/cursojava/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursojava.entities.User;
 import com.udemy.cursojava.repositories.UserRepository;
+import com.udemy.cursojava.services.exceptions.DataBaseException;
 import com.udemy.cursojava.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
